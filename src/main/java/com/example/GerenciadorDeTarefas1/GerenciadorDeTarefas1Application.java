ca342d383b110ab53a16ff150e80859968df2b1e
Gaaa1210 <gabrielccwanderley1@gmail.com>
2025-04-27 13:26:12 -0300

    Criação da entidade User, interface de acesso ao banco e implementação dos endpoints POST e GET para /users
    - Criada a entidade User com campos id, nome e email.
    - Implementada a interface UserRepository para interação com o banco de dados (H2).
    - Criado o serviço UserService para lógica de negócios dos usuários.
    - Implementados os endpoints GET e POST para /users no controller.
    - Configuração do banco de dados H2 já realizada e funcional.

--- src/main/java/com/example/GerenciadorDeTarefas1/GerenciadorDeTarefas1Application.java
+++ src/main/java/com/example/GerenciadorDeTarefas1/GerenciadorDeTarefas1Application.java
@@ -9,5 +9,4 @@ public class GerenciadorDeTarefas1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorDeTarefas1Application.class, args);
 	}
-
 }
--- src/main/java/com/example/GerenciadorDeTarefas1/user/User.java
+++ src/main/java/com/example/GerenciadorDeTarefas1/user/User.java
@@ -1,3 +1,31 @@
+//User é responsavel por mapear os campos que serão armazenados no banco de dados.
+
 package com.example.GerenciadorDeTarefas1.user;
+
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
+@Table(name = "users")
+@Entity
+@Data //Esta é uma função do lombok, ela faz aquela parte de set e get que eu fazia manualmente: ex (public Long getId() {
+        //return id;  // cuidado com maiúscula: 'id' e não 'Id'
+    //}
+
+//public void setId(Long id) {
+    //this.id = id;
+//})
+
 public class User {
+
+    @Id //Esta parte do codigo diz que: O campo é a chave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Esta linha fala para o banco de dados gerar o ID automaticamente
+    private Long id;
+
+    private String email;
+    private String name;
+
 }

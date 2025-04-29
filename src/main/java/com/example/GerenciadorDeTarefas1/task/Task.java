2010242b571f00fa9281b9e5e640c1c37230e0d5
Jefferson <jeffersonjunior1645@gmail.com>
2025-04-29 02:30:38 -0300

    feat: implementation of the Task module with CRUD and linking to the User/Category

--- src/main/java/com/example/GerenciadorDeTarefas1/task/Task.java
+++ src/main/java/com/example/GerenciadorDeTarefas1/task/Task.java
@@ -1,20 +1,35 @@
 package com.example.GerenciadorDeTarefas1.task;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.GerenciadorDeTarefas1.category.domain.Category;
+import com.example.GerenciadorDeTarefas1.user.User;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Data
+@Getter
+@Setter
 public class Task {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
+    private String description;
+    private boolean completed;
 
-    private String titulo;
+    @ManyToOne
+    private User user;
 
-    private String descricao;
+    @ManyToOne
+    private Category category;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- src/main/java/com/example/GerenciadorDeTarefas1/task/TaskResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.GerenciadorDeTarefas1.task;
-
-public class TaskResponse {
-}
